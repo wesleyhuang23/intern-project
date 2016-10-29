92c0007527505b14545ef9c8c29e2f6076161163
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-10-29 17:00:31 -0600

    angular configuring

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,13 @@
 angular.module('internApp').controller('internCtrl', function($scope, service){
 
 $scope.storeProfile = function(fullName, tagline, imgUrl, bio){
-  service.storeProfile(fullName, tagline, imgUrl, bio);
+  $scope.profile = service.storeProfile(fullName, tagline, imgUrl, bio);
+  console.log($scope.profile);
 };
+
+$scope.friendData = service.friendData;
+console.log($scope.friendData);
+
+//$scope.storeProfile();
 
 });
--- js/service.js
+++ js/service.js
@@ -1,6 +1,11 @@
 angular.module('internApp').service('service', function(){
 
-var friendData = [];
+this.friendData = [{
+  fullName: "Wesley Huang",
+  tagline: "This is working",
+  imgUrl: "https://i.vimeocdn.com/portrait/2859117_300x300",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}];
 
 this.storeProfile = function(fn, tagline, imgUrl, bio){
   var profile = {};
@@ -8,9 +13,11 @@ this.storeProfile = function(fn, tagline, imgUrl, bio){
   profile.tagline = tagline;
   profile.imgUrl = imgUrl;
   profile.bio = bio;
-  friendData.push(profile);
-  console.log(friendData);
-  alert('Congratulations! We successfully created your profile.');
+  this.friendData.push(profile);
+  console.log(this.friendData);
+  return this.friendData;
 };
+
+
 
 });
